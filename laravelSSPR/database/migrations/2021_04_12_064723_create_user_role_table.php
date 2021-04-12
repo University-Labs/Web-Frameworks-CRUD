97a4237c9f91507d1f553fa0b25baeb7781d2278
Evster <8961240@gmail.com>
2021-04-12 13:52:53 +0700

    Added migrations and traits for roles and permissions

---
+++ laravelSSPR/database/migrations/2021_04_12_064723_create_user_role_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_role', function (Blueprint $table) {
+            //ключ пользователя
+            $table->unsignedBigInteger('user_id');
+            //ключ роли
+            $table->unsignedBigInteger('role_id');
+            $table->timestamps();
+            // внешний ключ, ссылается на поле id таблицы users
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')->onDelete('cascade');
+            // внешний ключ, ссылается на поле id таблицы roles
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+            // составной первичный ключ
+            $table->primary(['user_id', 'role_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_role');
+    }
+}
