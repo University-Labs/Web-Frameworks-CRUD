3c2f07f4503553bd342588010b75cab8595004ca
Evster <8961240@gmail.com>
2021-04-14 15:27:36 +0700

    Moved routes into correct middleware groups

--- laravelSSPR/routes/web.php
+++ laravelSSPR/routes/web.php
@@ -37,41 +37,48 @@ Route::group(['middleware' => 'auth'], function() {
 		Route::get('/avtofirmlist', [AvtofirmController::class, 'avtofirmlist'])->name('avtofirms.list');
 	});
 
-	Route::delete('/deletecar_{id}', [CarController::class, 'deleteCar'])->name('cars.delete');
+	Route::group(['middleware' => 'perm:catalog-update'], function(){
+		Route::delete('/deletecar_{id}', [CarController::class, 'deleteCar'])->name('cars.delete');
 
-	Route::get('createcar', [CarController::class, 'createcar'])->name('cars.create');
+		Route::get('createcar', [CarController::class, 'createcar'])->name('cars.create');
 
-	Route::post('createcar', [CarController::class, 'storecar'])->name('cars.store');
+		Route::post('createcar', [CarController::class, 'storecar'])->name('cars.store');
 
-	Route::get('editcar_{id}', [CarController::class, 'editcar'])->name('cars.edit');
+		Route::get('editcar_{id}', [CarController::class, 'editcar'])->name('cars.edit');
 
-	Route::post('updatecar_{id}', [CarController::class, 'updatecar'])->name('cars.update');
+		Route::post('updatecar_{id}', [CarController::class, 'updatecar'])->name('cars.update');
 
+	});
 
+	Route::group(['middleware' => 'perm:goods-add'], function(){
 
-	Route::get('/avtocategorycreate', [CategoryController::class, 'avtocategorycreate'])->name('categories.create');
-	Route::post('/avtocategorycreate', [CategoryController::class, 'storeavtocategory'])->name('categories.store');
-	Route::delete('/avtocategorydelete_{id}', [CategoryController::class, 'deleteavtocategory'])->name('categories.delete');
-	Route::get('/avtocategoryedit_{id}', [CategoryController::class, 'avtocategoryedit'])->name('categories.edit');
-	Route::post('/avtocategoryupdate_{id}', [CategoryController::class, 'updateavtocategory'])->name('categories.update');
+	});
 
-	Route::get('/baseavtocreate', [BaseavtoController::class, 'baseavtocreate'])->name('bases.create');
-	Route::post('/baseavtocreate', [BaseavtoController::class, 'storebaseavto'])->name('bases.store');
-	Route::delete('/baseavtodelete_{id}', [BaseavtoController::class, 'deletebaseavto'])->name('bases.delete');
-	Route::get('/baseavtoedit_{id}', [BaseavtoController::class, 'baseavtoedit'])->name('bases.edit');
-	Route::post('/baseavtoupdate_{id}', [BaseavtoController::class, 'updatebaseavto'])->name('bases.update');
+	Route::group(['middleware' => 'perm:dicts-update'], function(){
+		Route::get('/avtocategorycreate', [CategoryController::class, 'avtocategorycreate'])->name('categories.create');
+		Route::post('/avtocategorycreate', [CategoryController::class, 'storeavtocategory'])->name('categories.store');
+		Route::delete('/avtocategorydelete_{id}', [CategoryController::class, 'deleteavtocategory'])->name('categories.delete');
+		Route::get('/avtocategoryedit_{id}', [CategoryController::class, 'avtocategoryedit'])->name('categories.edit');
+		Route::post('/avtocategoryupdate_{id}', [CategoryController::class, 'updateavtocategory'])->name('categories.update');
 
-	Route::get('/superstructurecreate', [SuperstructureController::class, 'superstructurecreate'])->name('superstructures.create');
-	Route::post('/superstructurecreate', [SuperstructureController::class, 'storesuperstructure'])->name('superstructures.store');
-	Route::delete('/superstructuredelete_{id}', [SuperstructureController::class, 'deletesuperstructure'])->name('superstructures.delete');
-	Route::get('/superstructureedit_{id}', [SuperstructureController::class, 'superstructureedit'])->name('superstructures.edit');
-	Route::post('/superstructureupdate_{id}', [SuperstructureController::class, 'updatesuperstructure'])->name('superstructures.update');
+		Route::get('/baseavtocreate', [BaseavtoController::class, 'baseavtocreate'])->name('bases.create');
+		Route::post('/baseavtocreate', [BaseavtoController::class, 'storebaseavto'])->name('bases.store');
+		Route::delete('/baseavtodelete_{id}', [BaseavtoController::class, 'deletebaseavto'])->name('bases.delete');
+		Route::get('/baseavtoedit_{id}', [BaseavtoController::class, 'baseavtoedit'])->name('bases.edit');
+		Route::post('/baseavtoupdate_{id}', [BaseavtoController::class, 'updatebaseavto'])->name('bases.update');
 
-	Route::get('/avtofirmcreate', [AvtofirmController::class, 'avtofirmcreate'])->name('avtofirms.create');
-	Route::post('/avtofirmcreate', [AvtofirmController::class, 'storeavtofirm'])->name('avtofirms.store');
-	Route::delete('/avtofirmdelete_{id}', [AvtofirmController::class, 'deleteavtofirm'])->name('avtofirms.delete');
-	Route::get('/avtofirmedit_{id}', [AvtofirmController::class, 'avtofirmedit'])->name('avtofirms.edit');
-	Route::post('/avtofirmupdate_{id}', [AvtofirmController::class, 'updateavtofirm'])->name('avtofirms.update');
+		Route::get('/superstructurecreate', [SuperstructureController::class, 'superstructurecreate'])->name('superstructures.create');
+		Route::post('/superstructurecreate', [SuperstructureController::class, 'storesuperstructure'])->name('superstructures.store');
+		Route::delete('/superstructuredelete_{id}', [SuperstructureController::class, 'deletesuperstructure'])->name('superstructures.delete');
+		Route::get('/superstructureedit_{id}', [SuperstructureController::class, 'superstructureedit'])->name('superstructures.edit');
+		Route::post('/superstructureupdate_{id}', [SuperstructureController::class, 'updatesuperstructure'])->name('superstructures.update');
+
+		Route::get('/avtofirmcreate', [AvtofirmController::class, 'avtofirmcreate'])->name('avtofirms.create');
+		Route::post('/avtofirmcreate', [AvtofirmController::class, 'storeavtofirm'])->name('avtofirms.store');
+		Route::delete('/avtofirmdelete_{id}', [AvtofirmController::class, 'deleteavtofirm'])->name('avtofirms.delete');
+		Route::get('/avtofirmedit_{id}', [AvtofirmController::class, 'avtofirmedit'])->name('avtofirms.edit');
+		Route::post('/avtofirmupdate_{id}', [AvtofirmController::class, 'updateavtofirm'])->name('avtofirms.update');
+	});
 
 });
 
