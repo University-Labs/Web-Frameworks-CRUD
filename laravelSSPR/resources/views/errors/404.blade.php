3568451ffab9f715935a84cbc868f12f3ad42a7c
Evster <8961240@gmail.com>
2021-04-14 15:43:56 +0700

    Added 404 page

---
+++ laravelSSPR/resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+
+@extends('layouts.layout')
+
+@section('title')
+	Категории авто
+@endsection
+
+@section('content')
+
+<main id="content">
+	<div class="container text-center">
+		<h1> Страница не найдена (Ошибка 404) </h1>
+		<h5> Вы точно сюда хотели попасть? </h5>
+        <div class="row">
+          <div class="col-lg-11">
+            <img src="img/404.jpg" class="img-fluid">
+          </div>
+        </div>
+	</div>
+</main>
+
+@endsection
